17834f91b8c7bb68418c2d753c02e144cba1ccaa
HuuHieu <huuhieu2611@gmail.com>
2025-02-23 18:05:35 +0700

    chua hoan thien

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -1,9 +1,22 @@
-#incldue <iostream>
-
+#include <iostream>
+#include <cstring>
 using namespace std;
 
+struct quan_ly_du_an{
+	string name;
+	float birth,id;
+
+};
+void nhap_du_lieu(const quan_ly_du_an &n){
+	cout <<"Nhap vao du lieu cua ban : \n";
+	cout <<"Ten nguoi dung : \n";
+	cin >> n.name;
+	cout <<"Ngay thang nam sinh : \n";
+	cin >> n.birth;
+	cout <<"Nhap id cua ban : \n ";
+	cin >> n.id;
+}
 int main (){
-	
-	cout <<" Hello World ";
+	nhap_du_lieu();
 	return 0;
 }
