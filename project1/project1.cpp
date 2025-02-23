698b7dff8310a6049599bf20717c25c36cdb3afd
HuuHieu <huuhieu2611@gmail.com>
2025-02-23 18:10:22 +0700

    fix

--- project1/project1.cpp
+++ project1/project1.cpp
@@ -2,21 +2,22 @@
 #include <cstring>
 using namespace std;
 
-struct quan_ly_du_an{
+struct quan_ly_du_an{ // chua done cau truc 
 	string name;
 	float birth,id;
 
 };
-void nhap_du_lieu(const quan_ly_du_an &n){
+
+void nhap_du_lieu(quan_ly_du_an &n){     // chua qua tai nhap
 	cout <<"Nhap vao du lieu cua ban : \n";
 	cout <<"Ten nguoi dung : \n";
-	cin >> n.name;
+	getline(cin, n.name);
 	cout <<"Ngay thang nam sinh : \n";
 	cin >> n.birth;
 	cout <<"Nhap id cua ban : \n ";
 	cin >> n.id;
-}
-int main (){
+
+int main (){s
 	nhap_du_lieu();
 	return 0;
 }
